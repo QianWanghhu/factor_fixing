3e990638b37bce9f850920e3c587df52a148cf08
Qian Wang <qian@hhu.edu.cn>
2020-08-05 08:27:04 +1000

    Clean up

--- qian_dev/sa_product_dist.py
+++ qian_dev/sa_product_dist.py
@@ -12,30 +12,7 @@ from basic.boots_pya import least_squares, fun, pce_fun
 from basic.utils import variables_prep, names_match
 from basic.partial_rank import partial_rank
 
-fpath = 'D:/cloudStor/Research/pce_fixing/pyfile/pya_related/'
-filename = f'{fpath}parameter-adjust.csv'
-variable = variables_prep(filename, product_uniform=True)
-filename = f'{fpath}samples_adjust.csv'
-data = np.loadtxt(filename,delimiter=",",skiprows=1)[:,1:]
-samples = data[:,:11].T
-values = data[:,11:]
-values = values[:,:1]# focus on first qoi
-ntrain_samples = 198
-poly, error = pce_fun(variable, samples, values, ntrain_samples, degree=2)
-
-# with adjustment to parameter ranges
-index_product = np.array([[1, 0, 2, 3, 9, 10, 11, 16, 17], 
-                         [6, 5, 7], 
-                         [20, 19],
-                         ])
-fpath = 'D:/cloudStor/Research/pce_fixing/pyfile/pya_related/'
-
-problem = read_param_file(f'{fpath}problem.txt', delimiter=',')
-filename = f'{fpath}problem_adjust.txt'
-problem_adjust = read_param_file(filename, delimiter=',')
-problem = read_param_file(f'{fpath}problem.txt', delimiter=',')
-
-def samples_df(sample_method='samples_product', Nsamples = 2000):
+def samples_df(sample_method = 'samples_product', Nsamples = 2000):
     def samples_product(Nsamples=Nsamples):
         sample_new = np.round(saltelli.sample(problem, 
                                         N=Nsamples, 
@@ -47,7 +24,7 @@ def samples_df(sample_method='samples_product', Nsamples = 2000):
                                         seed=121), 4)
         return sample_new
 
-    # sampling with the only ranges of parameters changed
+    # sampling for parameters with the only ranges changed and the distributions kept as uniform
     def ranges_adjust(Nsamples=Nsamples):
         sample_range_change = np.round(saltelli.sample(problem_adjust, 
                                         N=Nsamples, 
@@ -61,9 +38,31 @@ def samples_df(sample_method='samples_product', Nsamples = 2000):
         df = ranges_adjust()
     return df
 
-# expand the samples with other parameters set to a fixed value
-rankings = {}
-rank_names = {}
+# expand the samples with other parameters fixed
+filepath = '../data/'
+filename = f'{filepath}parameter-adjust.csv'
+variable = variables_prep(filename, product_uniform=True)
+
+filepath = '../output/paper/'
+filename = f'{filepath}samples_adjust.csv'
+data = np.loadtxt(filename, delimiter=",", skiprows=1)[:,1:]
+samples = data[:,:11].T
+values = data[:,11:]
+values = values[:,:1]# focus on first qoi
+ntrain_samples = 198
+poly, error = pce_fun(variable, samples, values, ntrain_samples, degree=2)
+
+# with adjustment to parameter ranges
+index_product = np.array([[1, 0, 2, 3, 9, 10, 11, 16, 17], 
+                         [6, 5, 7], 
+                         [20, 19],
+                         ])
+# import problem for SALib
+problem = read_param_file(f'{fpath}problem.txt', delimiter=',')
+filename = f'{filepath}problem_adjust.txt'
+problem_adjust = read_param_file(filename, delimiter=',')
+rankings, rank_names = {}, {}
+
 for n in range(200, 2001, 200):
     df = samples_df(sample_method='samples_product', Nsamples = n)
     y_range_change = np.round(poly(df.T), 4).reshape(list(df.shape)[0])
@@ -74,17 +73,8 @@ for n in range(200, 2001, 200):
     rankings[f'nsample_{n}'] = partial_rank(total_resample.T, problem_adjust['num_vars'])
     rank_names[f'nsample_{n}'] = names_match(rankings[f'nsample_{n}'], problem_adjust['names'])
 
-
+sa = {key: value for key, value in sa.items() if 'ci' not in key}
 sa_df = pd.DataFrame.from_dict(sa)
 sa_df.index = problem_adjust['names']
-
-fpath_save = 'D:/cloudStor/Research/pce_fixing/output/linear_dep/'
+fpath_save = '../output/paper/'
 sa_df.to_csv(f'{fpath_save}sa_samples_product.csv', index=True)
-# plot using plot in SALib
-sns.set(style="whitegrid")
-rc("text", usetex=False)
-fig = plt.figure(figsize=(6, 4))
-ax = barplot(sa_df)
-ax.set_xlabel('Parameters')
-ax.set_ylabel('Main / Total effects');
-plt.savefig(f'{fpath_save}sampling_product.png', format='png', dpi=300)
