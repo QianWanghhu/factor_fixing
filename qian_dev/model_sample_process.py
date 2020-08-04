ca52316c5186b5f7027edf451b4fe705ecd75cee
Qian Wang <qian@hhu.edu.cn>
2020-08-05 08:27:51 +1000

    process model outputs

--- qian_dev/model_sample_process.py
+++ qian_dev/model_sample_process.py
@@ -1,5 +1,8 @@
-import numpy as np
+# Script for preparing training dataset for PCE
+# import packages
 import pandas as pd
+import numpy as np
+import os
 import pyapprox as pya
 from pyapprox.probability_measure_sampling import generate_independent_random_samples
 from scipy.stats import beta, uniform, norm as beta, uniform, norm
@@ -7,9 +10,30 @@ from basic.utils import variables_prep
 from scipy import stats
 from SALib.util import read_param_file
 
-def main():
-    fpath_save = 'output/paper/'
+# read model results
+def samples_combine():
+    filepath = 'output/Run0730/'
+    # combine TSS results into a file
+    filenames = os.listdir(filepath)
+    for fn in filenames:
+        if 'Tss' in fn:
+            tss_temp = pd.read_csv(f'{filepath}{fn}', index_col='# Date')
+            tss_temp.index = pd.to_datetime(tss_temp.index)
+            tss_temp = tss_temp.loc['2000-07-01':'2014-06-30',:]
+            tss_temp.dropna(axis=1, how='any', inplace=True)
+            try:
+                f_quantile = np.hstack((f_quantile, tss_temp.sum(axis = 0).values / 14))
+            except NameError:
+                f_quantile = tss_temp.sum(axis = 0).values / 14
+    file_sample = [fn for fn in filenames if 'sample' in fn]
+    f_train = pd.read_csv(f'{filepath}{file_sample[0]}', index_col= 'index')
+    f_train.loc[:, 'ave_annual'] = f_quantile
+    f_train.to_csv(f'{filepath}2000_2014_ave_annual.csv', index_label='id')
+
+def main():    
+    samples_combine()
     # import samples and values
+    fpath_save = 'output/paper/'
     fpath = 'output/Run0730/'
     filename = f'{fpath}2000_2014_ave_annual.csv'
     data = np.loadtxt(filename,delimiter=",",skiprows=1)[:,1:]
@@ -19,8 +43,6 @@ def main():
     # import parameter inputs and generate the dataframe of analytical ratios between sensitivity indices
     fpath_input = 'data/'
     filename = f'{fpath_input}parameter-reimplement.csv'
-    # variable = variables_prep(filename, product_uniform=False)
-    # param_all = pd.read_csv(filename).loc[:, 'Veneer_name'].values
     index_product = np.array([[1, 0, 2, 3, 9, 10, 11, 16, 17], 
                             [6, 5, 7], 
                             [19, 20],
@@ -41,36 +63,12 @@ def main():
         # samples_adjust[index_temp[1:], :] = 1
         pars_delete.extend(index_temp[1:])
     samples_adjust = np.delete(samples_adjust, pars_delete, axis=0)
-    # import pdb
-    # pdb.set_trace()
 
     samples_adjust = np.append(samples_adjust, [values.flatten()], axis=0)    
     df_adjust = pd.DataFrame(data = samples_adjust.T, 
                             index = np.arange(samples_adjust.shape[1]),
                             columns = [*param_adjust.loc[:, 'Veneer_name'], 'TSS_ave'])
     df_adjust.to_csv(f'{fpath_save}samples_adjust.csv')
-
-    # # Used for PCE fitting
-    # ntrain_samples = 552
-    # nboot = 1000
-    # if ntrain_samples == 552:
-    #     error_cv, error_bt, main_effects, total_effects = fun(variable, samples, 
-    #                                                 values, degree=2, nboot=nboot, 
-    #                                                 ntrain_samples=ntrain_samples)
-    #     sa_df = df_format(main_effects, total_effects, variable, 
-    #                     param_all, conf_level=0.95)
-    #     sa_df.to_csv(f'{fpath_save}sa_pce_raw.csv', index=True)
-    #     plot(sa_df, sav=True, figname='sa_PCE_Beta_degree2_uniform.png')
-    # else:
-    #     poly_beta, error = pce_fun(variable_adjust, samples_adjust, 
-    #                     values, ntrain_samples, degree=2)
-    #     approx_values = poly_beta(samples_adjust)                    
-    #     error_vali = np.linalg.norm(approx_values - values) / np.linalg.norm(values)
-
-    # total_effects = np.array(total_effects)
-    # total_effects = total_effects.reshape(nboot, variable_adjust.num_vars())
-    # rankings = partial_rank(total_effects, variable_adjust.num_vars())
-    # rank_names = names_match(rankings, param_adjust.loc[:, 'Veneer_name'].values)
 
 if __name__ == "__main__":
     main()
