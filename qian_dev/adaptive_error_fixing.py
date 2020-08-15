46ba58b79e00cdf1ceaec9d2aa3c80567ef94c6e
Qian Wang <qian@hhu.edu.cn>
2020-08-15 15:50:14 +1000

    Adjust the default values to fix at after reparameterization

--- qian_dev/adaptive_error_fixing.py
+++ qian_dev/adaptive_error_fixing.py
@@ -10,16 +10,17 @@ from SALib.util import read_param_file
 from basic.boots_pya import fun, pce_fun
 from basic.utils import variables_prep, to_df, adjust_sampling
 from basic.group_fix import group_fix
+from sa_product_dist import samples_df
 
 # import variables and samples for PCE
-file_list = ['parameter-adjust', 'samples_adjust', 'partial_reduce_beta', 'partial_reduce_params']
+file_list = ['parameter-adjust', 'samples_adjust', 'partial_reduce_beta_552', 'partial_reduce_params']
 input_path = '../data/'
 filename = f'{input_path}{file_list[0]}.csv'
 variable = variables_prep(filename, product_uniform=True)
 
 output_path = '../output/paper/'
 filename = f'{output_path}{file_list[1]}.csv'
-data = np.loadtxt(filename,delimiter=",",skiprows=1)[:,1:]
+data = np.loadtxt(filename, delimiter=",",skiprows=1)[:,1:]
 len_params = variable.num_vars()
 samples = data[:,:len_params].T
 values = data[:,len_params:]
@@ -35,8 +36,12 @@ index_product = np.load(f'{input_path}index_product.npy', allow_pickle=True)
 # generate samples for error metric analysis
 filename = f'{input_path}problem.txt'
 problem = read_param_file(filename, delimiter=',')
+# filename = f'{input_path}problem_adjust.txt'
+# problem_adjust = read_param_file(filename, delimiter=',')
 # Define default values to fix parameters at
 x_fix = np.array(problem['bounds']).mean(axis=1).reshape((problem['num_vars'], 1))
+if (variable.num_vars()) == 11:
+    x_fix_adjust = adjust_sampling(x_fix, index_product, x_fix)
 # x_fix = np.ones(shape=(problem['num_vars'], 1))
 
 partial_order = dict((key, value) for key, value in partial_order.items() if str(156) in key)
@@ -46,15 +51,16 @@ value = partial_order[key]
 poly, error = pce_fun(variable, samples, values, 
                     ntrain_samples=int(sample_size), degree=2)
 
-nstart, nstop, nstep = 10000, 12000, 500
+nstart, nstop, nstep = 100, 1000, 100
 conf_uncond, error_dict, pool_res, y_uncond = {}, {}, {}, {}
 for n in range(nstart, nstop + 1, nstep):
     x_sample = latin.sample(problem, n, seed=88)
     x_sample = x_sample.T
-    # if reduce parameters, change samples
+    
+    # # if reduce parameters, change samples
     if (variable.num_vars()) == 11:
         x_sample = adjust_sampling(x_sample, index_product, x_fix)
-
+    
     # Calculate the corresponding number of bootstrap with use pf group_fix
     # the adaptive process requires PCE to be fitted with increasing sample size
     # therefore the calculation is done without avoiding repeating analysis
@@ -62,12 +68,13 @@ for n in range(nstart, nstop + 1, nstep):
     # add the calculation of y_uncond
     y_uncond[str(n)] = poly(x_sample).flatten()
         # conf_uncond[str(n)] = np.quantile(y_uncond[key], [0.025, 0.975])
-    error_dict[str(n)], pool_res = group_fix(value, poly, x_sample, y_uncond[str(n)], x_fix, rand, {}, file_exist=True)
+    error_dict[str(n)], pool_res = group_fix(value, poly, x_sample, y_uncond[str(n)], 
+                                    x_fix_adjust, rand, {}, file_exist=True)
     # End for
 
     
 # separate confidence intervals into separate dicts and write results
-save_path = f'{output_path}error_measures/'
+save_path = f'{output_path}error_measures3/'
 if not os.path.exists(save_path): os.mkdir(save_path)
 # convert the result into dataframe
 key_outer = list(error_dict.keys())
@@ -75,4 +82,4 @@ f_names = list(error_dict[key_outer[0]].keys())
 for ele in f_names:
     dict_measure = {key: error_dict[key][ele] for key in key_outer}
     df = pd.DataFrame.from_dict(dict_measure)
-    df.to_csv(f'{save_path}/{ele}_adaptive2.csv')
+    df.to_csv(f'{save_path}/{ele}_adaptive.csv')
