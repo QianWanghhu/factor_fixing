bce12326319a511da96d592da4d871924df9dea3
Qian Wang <qian@hhu.edu.cn>
2020-08-18 10:10:42 +1000

    Add the calculation of unconditional median

--- qian_dev/adaptive_error_fixing.py
+++ qian_dev/adaptive_error_fixing.py
@@ -10,7 +10,7 @@ from SALib.util import read_param_file
 from basic.boots_pya import fun, pce_fun
 from basic.utils import variables_prep, to_df, adjust_sampling
 from basic.group_fix import group_fix
-from sa_product_dist import samples_df
+# from sa_product_dist import samples_df
 
 # import variables and samples for PCE
 file_list = ['parameter-adjust', 'samples_adjust', 'partial_reduce_beta_552', 'partial_reduce_params']
@@ -36,8 +36,6 @@ index_product = np.load(f'{input_path}index_product.npy', allow_pickle=True)
 # generate samples for error metric analysis
 filename = f'{input_path}problem.txt'
 problem = read_param_file(filename, delimiter=',')
-# filename = f'{input_path}problem_adjust.txt'
-# problem_adjust = read_param_file(filename, delimiter=',')
 # Define default values to fix parameters at
 x_fix = np.array(problem['bounds']).mean(axis=1).reshape((problem['num_vars'], 1))
 if (variable.num_vars()) == 11:
@@ -51,9 +49,10 @@ value = partial_order[key]
 poly, error = pce_fun(variable, samples, values, 
                     ntrain_samples=int(sample_size), degree=2)
 
-nstart, nstop, nstep = 100, 1000, 100
+nstart, nstop, nstep = 1000, 10000, 500
 conf_uncond, error_dict, pool_res, y_uncond = {}, {}, {}, {}
 for n in range(nstart, nstop + 1, nstep):
+    print(n)
     x_sample = latin.sample(problem, n, seed=88)
     x_sample = x_sample.T
     
@@ -67,14 +66,14 @@ for n in range(nstart, nstop + 1, nstep):
     rand = np.random.randint(0, x_sample.shape[1], size=(1000, x_sample.shape[1]))
     # add the calculation of y_uncond
     y_uncond[str(n)] = poly(x_sample).flatten()
-        # conf_uncond[str(n)] = np.quantile(y_uncond[key], [0.025, 0.975])
+    conf_uncond[str(n)]  = np.median(y_uncond[str(n)])       
     error_dict[str(n)], pool_res = group_fix(value, poly, x_sample, y_uncond[str(n)], 
                                     x_fix_adjust, rand, {}, file_exist=True)
     # End for
 
     
 # separate confidence intervals into separate dicts and write results
-save_path = f'{output_path}error_measures3/'
+save_path = f'{output_path}error_measures/'
 if not os.path.exists(save_path): os.mkdir(save_path)
 # convert the result into dataframe
 key_outer = list(error_dict.keys())
@@ -83,3 +82,6 @@ for ele in f_names:
     dict_measure = {key: error_dict[key][ele] for key in key_outer}
     df = pd.DataFrame.from_dict(dict_measure)
     df.to_csv(f'{save_path}/{ele}_adaptive.csv')
+
+df_medain = pd.Series(conf_uncond).to_frame('median').T
+df_medain.to_csv(f'{save_path}/median_uncond_adaptive.csv')
