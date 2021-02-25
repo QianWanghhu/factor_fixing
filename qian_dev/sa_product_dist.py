985969f897a407eb03193c5e1de44f23bc0e252e
Qian Wang <qian@hhu.edu.cn>
2021-02-25 11:46:01 +0800

    Use new settings of importing data

--- qian_dev/sa_product_dist.py
+++ qian_dev/sa_product_dist.py
@@ -1,3 +1,6 @@
+"""
+Script used to generate the sampling-based total-order effects.
+"""
 import numpy as np
 import pandas as pd
 from scipy.stats import uniform
@@ -9,6 +12,7 @@ import time
 from basic.boots_pya import fun, pce_fun
 from basic.utils import variables_prep, names_match
 from basic.partial_rank import partial_rank
+from basic.read_data import file_settings, read_specify
 
 start_time = time.time()
 def samples_df(sample_method = 'samples_product', Nsamples = 2000):
@@ -38,34 +42,28 @@ def samples_df(sample_method = 'samples_product', Nsamples = 2000):
     return df
 
 # expand the samples with other parameters fixed
-input_path = '../data/'
-filename = f'{input_path}parameter-adjust.csv'
-variable = variables_prep(filename, product_uniform=False)
-
-filepath = '../output/paper0915/'
-filename = f'{filepath}samples_adjust.csv'
-data = np.loadtxt(filename, delimiter=",", skiprows=1)[:,1:]
-samples = data[:,:11].T
-values = data[:,11:]
-values = values[:,:1]
-ntrain_samples = 156
-
+input_path = file_settings()[1]
+variable, _ = read_specify('parameter', 'reduced', product_uniform=False, num_vars=11)
+output_path = file_settings()[0]
+samples, values = read_specify('model', 'reduced', product_uniform=False, num_vars=11)
 # with adjustment to parameter ranges
 index_product = np.load(f'{input_path}index_product.npy', allow_pickle=True)
 # import problem for SALib
 problem = read_param_file(f'{input_path}problem.txt', delimiter=',')
 filename = f'{input_path}problem_adjust.txt'
 problem_adjust = read_param_file(filename, delimiter=',')
-rankings, rank_names = {}, {}
 
-nboot = 500
-rand = np.random.randint(0, ntrain_samples, size=(nboot, ntrain_samples))
+num_pce = 500
+seed = 222
+ntrain_samples = 156
+np.random.seed(seed)
+rand = np.random.randint(0, ntrain_samples, size=(num_pce, ntrain_samples))
 for n in range(600, 601, 100):
     print(n)
     df = samples_df(sample_method='samples_product', Nsamples = n)
-    for ii in range(nboot):
-        poly, error = pce_fun(variable, samples, values, 
-                            ntrain_samples, degree=2, boot_ind=rand[ii])
+    for ii in range(num_pce):
+        poly = fun(variable, samples, values, degree=2, 
+            nboot=1, I=rand[ii], ntrain_samples=ntrain_samples)
         y_range_change = np.round(poly(df.T), 4).reshape(list(df.shape)[0])
         sa, main_resample, total_resample = sobol.analyze(problem_adjust, 
                                         y_range_change, calc_second_order=False, 
@@ -74,23 +72,20 @@ for n in range(600, 601, 100):
             rankings = np.append(total_indices, total_resample, axis=1)
         except NameError:
             total_indices = total_resample[:]                                 
-    rankings[f'nsample_{n}'] = partial_rank(total_indices.T, problem_adjust['num_vars'])
-    rank_names[f'nsample_{n}'] = names_match(rankings[f'nsample_{n}'], problem_adjust['names'])
+    rankings = partial_rank(total_indices.T, problem_adjust['num_vars'])
+    rank_names = names_match(rankings, problem_adjust['names'])
     # End For
 # End For
 
 sa_df = pd.DataFrame(data = [total_indices.mean(axis=1), *np.quantile(total_indices, [0.025, 0.975], axis=1)],
                 index=['ST', 'ST_conf_lower', 'ST_conf_upper'], columns=problem_adjust['names']).T
 
-fpath_save = '../output/paper0915/'
-
 # save results of partial rankings for parameter index and names
-sa_df.to_csv(f'{filepath}sa_samples_product.csv', index=True)
-
-with open(f'{filepath}ranking_sampling.json', 'w') as fp:
+sa_df.to_csv(f'{output_path}sa_sampling.csv', index=True)
+with open(f'{output_path}ranking_sampling.json', 'w') as fp:
     json.dump(rankings, fp, indent=2)
 
-with  open(f'{filepath}ranking_sampling_name.json', 'w') as fp:
+with  open(f'{output_path}ranking_sampling_name.json', 'w') as fp:
     json.dump(rank_names, fp, indent=2)    
 
 print("--- %s seconds ---" % (time.time() - start_time))
