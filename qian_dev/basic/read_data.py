32ea21e8ebd3bbabf820e56ec76fcf6c34346ef4
Qian Wang <qian@hhu.edu.cn>
2021-02-25 11:50:36 +0800

    Use relative path from the root.

--- qian_dev/basic/read_data.py
+++ qian_dev/basic/read_data.py
@@ -6,8 +6,8 @@ import json
 from basic.utils import variables_prep  
 
 def file_settings():
-    model_dir = '../output/test/'
-    input_dir = '../data/'
+    model_dir = 'output/test/'
+    input_dir = 'data/'
     model_ts_full = f'{input_dir}2000_2014_ave_annual.csv'
     model_ts_reduced = f'{model_dir}samples_adjust.csv'
     
