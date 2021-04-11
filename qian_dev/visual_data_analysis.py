187a87fb19265148a5bf056d56aa5190d9171199
QianW <qian@hhu.edu.cn>
2021-04-11 15:15:52 +0800

    Clean up the code to combine total effects calculated by exact basis.

--- qian_dev/visual_data_analysis.py
+++ qian_dev/visual_data_analysis.py
@@ -3,39 +3,70 @@ import pandas as pd
 import seaborn as sns
 import json
 
-from basic.plot import short_name, df_read
+from basic.plot import short_name
+from basic.read_data import file_settings, read_specify
 
 # clean the dataframe ordered by the sampling-based sensitivity indices
-fpath_save = '../output/paper0915/'
-filename = ['sa_samples_product', 'sa_pce_uniform', 'sa_pce_beta']
-df_raw = df_read(fpath_save, f'{filename[1]}.csv', 'PCE-Uniform', 2)
-df_sampling = df_read(fpath_save, f'{filename[0]}.csv', 'Sampling', 0)
-df_sampling = df_sampling.sort_values(by='ST', ascending=False)
-df_beta = df_read(fpath_save, f'{filename[2]}.csv', 'PCE-Beta', 2)
+def read_total_effects(fpath_save, product_uniform):
+    if product_uniform == 'beta':
+        dist_type = 'beta'
+    elif product_uniform == 'exact':
+        dist_type = 'exact'
+    else:
+        dist_type = 'uniform'
 
-index_product = np.load('../data/index_product.npy', allow_pickle=True)
+    filename = f'adaptive-reduce-{dist_type}_552.npz'
+    fileread = np.load(f'{fpath_save}{filename}', allow_pickle=True)
+    return fileread
 
+def df_read(df, result_type, type_num):
+    _, parameters = read_specify('parameter', 'reduced', 'uniform', num_vars=11)
+    df.rename(columns={'Unnamed: 0' : 'Parameters'}, inplace=True)
+    df['Parameters'] = parameters
+    df['Type'] = result_type
+    df['Type_num'] = type_num
+    return df
+# End df_read()
+
+fpath_save = '../output/test/'
+
+# read total effects calculated by different PCE settings.
+nsample = 156
+fileread = read_total_effects(fpath_save, product_uniform='uniform')
+df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
+df_raw = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
+    data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
+df_raw = df_read(df_raw, 'PCE-Uniform', 2)
+
+fileread = read_total_effects(fpath_save, product_uniform='beta')
+df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
+df_beta = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
+    data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
+df_beta = df_read(df_beta, 'PCE-Beta', 2)
+
+fileread = read_total_effects(fpath_save, product_uniform='exact')
+df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
+df_exact = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
+    data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
+df_exact = df_read(df_exact, 'PCE-Exact', 2)
+
+# Combine the total effects into one set.
 # model_group and the type of calculation to the dataframe
-df_sampling['Model_group'] = np.arange(df_sampling.shape[0])
+df_exact['Model_group'] = np.arange(df_exact.shape[0])
 df_raw['Model_group'] = None
 
 # set parameter groups
-for ii in range(df_sampling.shape[0]):
-    param = df_sampling.Parameters[ii]
-    df_raw.loc[df_raw[df_raw.Parameters==param].index, 'Model_group'] = df_sampling.Model_group[ii]
-    df_beta.loc[df_beta[df_beta.Parameters==param].index, 'Model_group'] = df_sampling.Model_group[ii]
-# if ((df_raw.shape[0]) == (df_sampling.shape[0])) == True:
-#     df_plot = pd.concat([df_raw, df_sampling, df_beta])
+for ii in range(df_exact.shape[0]):
+    param = df_exact.Parameters[ii]
+    df_raw.loc[df_raw[df_raw.Parameters==param].index, 'Model_group'] = df_exact.Model_group[ii]
+    df_beta.loc[df_beta[df_beta.Parameters==param].index, 'Model_group'] = df_exact.Model_group[ii]
 
-# df_plot = df_plot.sort_values(by=['Model_group', 'Type_num', 'ST'], 
-#                             ascending=[True, True, False]).reset_index(drop=True)
-
-df_plot = df_sampling.filter(items=['Parameters', 'ST', 'ST_conf_lower', 'ST_conf_upper'])
-df_plot.rename(columns={'ST': 'ST_sampling'})
-df_plot['ST_Beta'], df_plot['ST_Beta_conf_lower'], df_plot['ST_Beta_conf_upper'] = \
-    df_beta.ST, df_beta.ST_conf_lower, df_beta.ST_conf_upper 
-df_plot['ST_Uniform'], df_plot['ST_Uniform_conf_lower'], df_plot['ST_Uniform_conf_upper'] = \
-    df_raw.ST, df_raw.ST_conf_lower, df_raw.ST_conf_upper
+df_plot = df_exact.filter(items=['Parameters', 'ST', 'ST_lower', 'ST_upper'])
+df_plot.rename(columns={'ST': 'ST_exact'})
+df_plot['ST_Beta'], df_plot['ST_Beta_lower'], df_plot['ST_Beta_upper'] = \
+    df_beta.ST, df_beta.ST_lower, df_beta.ST_upper 
+df_plot['ST_Uniform'], df_plot['ST_Uniform_lower'], df_plot['ST_Uniform_upper'] = \
+    df_raw.ST, df_raw.ST_lower, df_raw.ST_upper
 
 df_plot = short_name(df_plot)
 names_update = ['bankErosionCoeff', 'HillslopeFineSDR', 'Gully_Management_Practice_Factor']
@@ -43,34 +74,7 @@ new_short_name = ['BEC-R', 'HFSDR-R', 'GMPF-R']
 for ii in range(len(names_update)):
     df_plot.loc[df_plot[df_plot.Parameters==names_update[ii]].index, 'short_name'] = new_short_name[ii]
 
+df_plot = df_plot.sort_values(by=['ST'], 
+    ascending=[False]).reset_index(drop=True)
 # save df_plot
-df_plot.to_csv(f'{fpath_save}/sa_fig2.csv')
-
-
-# clean the dataframe ordered by the sampling-based sensitivity indices
-filename = ['sa_pce_raw', 'sa_samples_product']
-df_raw = df_read(fpath_save, f'{filename[0]}.csv', 'PCE', 1)
-df_sampling = df_read(fpath_save, f'{filename[1]}.csv', 'Sampling', 0)
-df_sampling = df_sampling.sort_values(by='ST', ascending=False)
-# model_group and the type of calculation to the dataframe
-df_sampling['Model_group'] = np.arange(df_sampling.shape[0])
-df_raw['Model_group'] = None
-
-for ii in range(df_sampling.shape[0]):
-    param = df_sampling.Parameters[ii]
-    df_raw.loc[df_raw[df_raw.Parameters==param].index, 'Model_group'] = df_sampling.Model_group[ii]
-
-for jj in index_product:
-    df_raw.loc[jj[1:], 'Model_group'] = df_raw.Model_group[jj[0]]
-
-df_plot = pd.concat([df_raw, df_sampling])
-df_plot = df_plot.sort_values(by=['Model_group', 'Type_num', 'ST'], ascending=[True, True, False]).reset_index(drop=True)
-df_plot = short_name(df_plot)
-
-names_update = ['bankErosionCoeff', 'HillslopeFineSDR', 'Gully_Management_Practice_Factor']
-new_short_name = ['BEC-R', 'HFSDR-R', 'GMPF-R']
-for ii in range(len(names_update)):
-    df_plot.loc[df_plot[(df_plot.Parameters==names_update[ii]) & 
-            (df_plot.Type=='Sampling')].index, 'short_name'] = new_short_name[ii]
-
-df_plot.to_csv(f'{fpath_save}/sa_fig1_0811.csv')
+df_plot.to_csv(f'{fpath_save}/sa_fig.csv')
