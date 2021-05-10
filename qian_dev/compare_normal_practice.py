c652156c5391d1161c8ab8ad93b23c0888fa33b6
QianW <qian@hhu.edu.cn>
2021-05-10 15:45:20 +0800

    Use new PCE-22 for comparison

--- qian_dev/compare_normal_practice.py
+++ qian_dev/compare_normal_practice.py
@@ -1,64 +1,52 @@
 import numpy as np
 import pandas as pd
-import json
-import os
 from SALib.util import read_param_file
-import time
+import pickle
+import json
 
-from basic.boots_pya import fun
-from basic.utils import to_df, adjust_sampling
-from basic.group_fix import group_fix, uncond_cal
 from basic.read_data import file_settings, read_specify
-
-# Calculate the corresponding number of bootstrap with use of group_fix
-ntrain_samples = 552
-num_pce = 500
-input_path = file_settings()[1]
-variable, _ = read_specify('parameter', 'full', product_uniform=False, num_vars=22)
+from basic.utils import read_total_effects, df_read
+from basic.plot import short_name
 
 output_path = file_settings()[0]
-samples, values = read_specify('model', 'full', product_uniform=False, num_vars=22)
+# read total effects calculated by different PCE settings.
+nsample = 390
+fileread = read_total_effects(output_path, product_uniform=False)
+df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
+df_raw = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
+    data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
+df_raw = df_read(df_raw, 'PCE-Uniform', 2, False, 22)
+df_raw.to_csv(f'{output_path}sa_pce_22.csv')
 
+# change this to product_uniform='exact' to use new polynomials
+product_uniform = False
+input_path = file_settings()[1]
+variable, _ = read_specify('parameter', 'full', 
+    product_uniform=product_uniform, num_vars=22)
+output_path = file_settings()[0]
+samples, values = read_specify('model', 'full', 
+    product_uniform=False, num_vars=22)
 # import index_prodcut which is a array defining the correlations between parameters
+index_product = np.load(f'{input_path}index_product.npy', allow_pickle=True)
 filename = f'{input_path}problem.txt'
 problem = read_param_file(filename, delimiter=',')
 x_fix = np.array(problem['bounds']).mean(axis=1).reshape((problem['num_vars'], 1))
-
-###=============CALCULATE THE ERROR METRCIS FROM FACTOR FIXING============###
 x_sample = np.loadtxt(f'{output_path}metric_samples.txt')
 
-pool_res = {}
-rand = np.random.randint(0, x_sample.shape[1], size=(500, x_sample.shape[1]))
-cv_temp = np.zeros(num_pce)
-rand_pce = np.random.randint(0, ntrain_samples, size=(num_pce, ntrain_samples))
-ci_bounds = [0.025, 0.975]
-# add the calculation of y_uncond
-y_temp = np.zeros(shape=(num_pce, x_sample.shape[1]))
-pce_list = []
-for i in range(rand_pce.shape[0]):
-    poly = fun(variable, samples[:, rand_pce[i]], values[rand_pce[i]], 
-        degree=2, nboot=1, ntrain_samples=ntrain_samples)
-        
-    pce_list.append(poly)
-    y_temp[i, :] = poly(x_sample).flatten()
-    cv_temp[i] = np.sqrt(poly.variance())[0] / poly.mean()[0]
-# add the calculation of y_uncond
+# Fixing parameters ranked by different PCEs 
+# and 1000 samples are used to calculate the uncertainty measures
+print(f'--------Calculate uncertainty measures due to FF with PCE-{product_uniform}--------')
+from error_fixing import fix_group_ranking
+key_use = [f'nsample_{ii}' for ii in [390]]
+dist_type = 'full'
+filename = f'adaptive-reduce-{dist_type}_552'
 
-y_uncond = y_temp
-conf_uncond= uncond_cal(y_uncond, ci_bounds, rand)
-conf_uncond['cv'] = cv_temp.mean()
-conf_uncond['cv_low'], conf_uncond['cv_up'] = \
-            np.quantile(cv_temp, ci_bounds)
-# End for
-
-# sensitivity with PCE-22
+num_pce = 100; seed=1000
 sa_raw = pd.read_csv(f'{output_path}sa_pce_22.csv')
 thresholds = [0.01, 0.1]
-for thsd in thresholds:
+for thsd in thresholds[1:]:
     ind_fix = list(sa_raw[sa_raw.ST<thsd].index) 
-    partial_order = {0: ind_fix}
-    error_dict, pool_res = group_fix(partial_order, pce_list, 
-        x_sample, y_uncond, x_fix, rand, {}, file_exist=False)
-
-    error_df = pd.DataFrame.from_dict(error_dict)
-    error_df.to_csv(f'{output_path}fix_threshold_{thsd}.csv')
+    partial_order = {f'nsample_{nsample}': {'0': ind_fix}}
+    fix_group_ranking(input_path, variable, output_path, samples, values,
+        partial_order, index_product, problem, x_fix, x_fix, 
+            num_pce, seed, 1000, product_uniform, filename)
--- qian_dev/visual_data_analysis.py
+++ qian_dev/visual_data_analysis.py
@@ -6,50 +6,22 @@ import json
 
 from basic.plot import short_name
 from basic.read_data import file_settings, read_specify
+from basic.utils import read_total_effects, df_read
 
-# clean the dataframe ordered by the sampling-based sensitivity indices
-def read_total_effects(fpath_save, product_uniform):
-    if product_uniform == 'beta':
-        dist_type = 'beta'
-    elif product_uniform == 'exact':
-        dist_type = 'exact'
-    else:
-        dist_type = 'uniform'
-
-    filename = f'adaptive-reduce-{dist_type}_552.npz'
-    fileread = np.load(f'{fpath_save}{filename}', allow_pickle=True)
-    return fileread
-
-def df_read(df, result_type, type_num):
-    _, parameters = read_specify('parameter', 'reduced', 'uniform', num_vars=11)
-    df.rename(columns={'Unnamed: 0' : 'Parameters'}, inplace=True)
-    df['Parameters'] = parameters
-    df['Type'] = result_type
-    df['Type_num'] = type_num
-    return df
-# End df_read()
-
-fpath_save = '../output/adaptive/'
-
+fpath_save = file_settings()[0]
 # read total effects calculated by different PCE settings.
 nsample = 90
 fileread = read_total_effects(fpath_save, product_uniform='uniform')
 df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
 df_raw = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
     data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
-df_raw = df_read(df_raw, 'PCE-Uniform', 2)
-
-# fileread = read_total_effects(fpath_save, product_uniform='beta')
-# df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
-# df_beta = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
-#     data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
-# df_beta = df_read(df_beta, 'PCE-Beta', 2)
+df_raw = df_read(df_raw, 'PCE-Uniform', 2, 'uniform', 11)
 
 fileread = read_total_effects(fpath_save, product_uniform='exact')
 df_temp = pd.DataFrame.from_dict(fileread[fileread.files[-1]][()][f'nsample_{nsample}']).T
 df_exact = pd.DataFrame(index=['ST', 'ST_lower', 'ST_upper'], columns=list(df_temp.index),
     data = [df_temp.mean(axis=1), df_temp.quantile(q=0.025, axis=1), df_temp.quantile(q=0.975, axis=1)]).T
-df_exact = df_read(df_exact, 'PCE-Exact', 2)
+df_exact = df_read(df_exact, 'PCE-Exact', 2, 'exact', 11)
 
 # Combine the total effects into one set.
 # model_group and the type of calculation to the dataframe
@@ -60,12 +32,10 @@ df_raw['Model_group'] = None
 for ii in range(df_exact.shape[0]):
     param = df_exact.Parameters[ii]
     df_raw.loc[df_raw[df_raw.Parameters==param].index, 'Model_group'] = df_exact.Model_group[ii]
-    # df_exact.loc[df_exact[df_exact.Parameters==param].index, 'Model_group'] = df_exact.Model_group[ii]
-
+    
 df_plot = df_exact.filter(items=['Parameters', 'ST', 'ST_lower', 'ST_upper'])
 df_plot.rename(columns={'ST': 'ST_exact'})
-# df_plot['ST_Beta'], df_plot['ST_Beta_lower'], df_plot['ST_Beta_upper'] = \
-#     df_beta.ST, df_beta.ST_lower, df_beta.ST_upper 
+
 df_plot['ST_Uniform'], df_plot['ST_Uniform_lower'], df_plot['ST_Uniform_upper'] = \
     df_raw.ST, df_raw.ST_lower, df_raw.ST_upper
 
